75ef7bcdc3e32abd410c5bd782d8be102516015f
Andrew Kelley <andrew@ziglang.org>
2023-10-23 11:35:45 -0700

    update to zig 0.12.0-dev.1239+ffa20ff72

--- README.md
+++ README.md
@@ -9,3 +9,11 @@ Here's a basic window with SDL2 in Zig.
 ```
 zig build run
 ```
+
+## Cross-Compiling
+
+Pass a `-Dtarget` option. For example:
+
+```
+zig build -Dtarget=x86_64-windows
+```
--- build.zig
+++ build.zig
@@ -1,16 +1,31 @@
 const Builder = @import("std").build.Builder;
 
 pub fn build(b: *Builder) void {
-    const mode = b.standardReleaseOptions();
-    const exe = b.addExecutable("sdl-zig-demo", "src/main.zig");
-    exe.setBuildMode(mode);
-    exe.linkSystemLibrary("SDL2");
-    exe.linkSystemLibrary("c");
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
 
-    b.default_step.dependOn(&exe.step);
+    const exe = b.addExecutable(.{
+        .name = "sdl-zig-demo",
+        .root_source_file = .{ .path = "src/main.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
+    if (target.isNativeOs() and target.getOsTag() == .linux) {
+        // The SDL package doesn't work for Linux yet, so we rely on system
+        // packages for now.
+        exe.linkSystemLibrary("SDL2");
+        exe.linkLibC();
+    } else {
+        const sdl_dep = b.dependency("sdl", .{
+            .optimize = .ReleaseFast,
+            .target = target,
+        });
+        exe.linkLibrary(sdl_dep.artifact("SDL2"));
+    }
+
     b.installArtifact(exe);
 
     const run = b.step("run", "Run the demo");
-    const run_cmd = exe.run();
+    const run_cmd = b.addRunArtifact(exe);
     run.dependOn(&run_cmd.step);
 }
